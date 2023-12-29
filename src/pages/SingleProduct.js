1b12ae672eafe77ed08bc11845e05d43eff4fc43
Somewhere <likeaboss13371@gmail.com>
2023-12-29 19:10:29 +0200

    Single Product Details Design Added

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -3,8 +3,18 @@ import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ProductCard from "../components/ProductCard";
 import ReactStars from "react-rating-stars-component";
+import ReactImageZoom from "react-image-zoom";
+import Color from "../components/Color";
+import { TbGitCompare } from "react-icons/tb";
+import { FaRegHeart } from "react-icons/fa6";
 
 const SingleProduct = () => {
+  const props = {
+    width: 400,
+    height: 600,
+    zoomWidth: 600,
+    img: "https://www.bartonwatchbands.com/cdn/shop/articles/Most_popular_luxury_watches.jpg?v=1686944605&width=1100",
+  };
   const [orderedProduct, setOrderedProduct] = useState(true);
   return (
     <>
@@ -13,8 +23,145 @@ const SingleProduct = () => {
       <div className="main-product-wrapper home-wrapper-2 py-5">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="main-product-image">
+                <div>
+                  <ReactImageZoom {...props} />
+                </div>
+              </div>
+              <div className="other-product-images d-flex flex-wrap gap-15">
+                <div>
+                  <img
+                    src="https://www.bartonwatchbands.com/cdn/shop/articles/Most_popular_luxury_watches.jpg?v=1686944605&width=1100"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://www.bartonwatchbands.com/cdn/shop/articles/Most_popular_luxury_watches.jpg?v=1686944605&width=1100"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://www.bartonwatchbands.com/cdn/shop/articles/Most_popular_luxury_watches.jpg?v=1686944605&width=1100"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+                <div>
+                  <img
+                    src="https://www.bartonwatchbands.com/cdn/shop/articles/Most_popular_luxury_watches.jpg?v=1686944605&width=1100"
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="main-product-details">
+                <div className="border-bottom">
+                  <h3 className="title">Some Product</h3>
+                </div>
+                <div className="border-bottom py-3">
+                  <p className="price">&#8372;100</p>
+                  <div className="d-flex align-items-center gap-10">
+                    <ReactStars
+                      count={5}
+                      size={24}
+                      value={3}
+                      edit={false}
+                      activeColor="#ffd700"
+                    />
+                    <p className="mb-0 t-review">(2 Reviews)</p>
+                  </div>
+                  <a href="#review" className="review-btn">
+                    Write a Review
+                  </a>
+                </div>
+                <div className="border-bottom py-3">
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Type:</h3>
+                    <p className="product-data">Some Type</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Brand:</h3>
+                    <p className="product-data">Some Brand</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Category:</h3>
+                    <p className="product-data">Watch</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Tags:</h3>
+                    <p className="product-data">Watch</p>
+                  </div>
+                  <div className="d-flex gap-10 align-items-center my-2">
+                    <h3 className="product-heading">Avaliability:</h3>
+                    <p className="product-data">In Stock</p>
+                  </div>
+                  <div className="d-flex gap-10 flex-column mt-2 mb-3">
+                    <h3 className="product-heading">Size:</h3>
+                    <div className="d-flex flex-wrap gap-15">
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        S
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        M
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        XL
+                      </span>
+                      <span className="badge border border-1 bg-white text-dark border-secondary">
+                        XXL
+                      </span>
+                    </div>
+                  </div>
+                  <div className="d-flex gap-10 flex-column mt-2 mb-3">
+                    <h3 className="product-heading">Color:</h3>
+                    <Color />
+                  </div>
+                  <div className="d-flex align-items-center gap-15 flex-row mt-2 mb-3">
+                    <h3 className="product-heading">Quantity:</h3>
+                    <div className="">
+                      <input
+                        type="number"
+                        name=""
+                        min={1}
+                        max={10}
+                        id=""
+                        className="form-control"
+                        style={{ width: "70px" }}
+                      />
+                    </div>
+                    <div className="d-flex align-items-center gap-30 ms-5">
+                      <button className="button border-0" type="submit">
+                        Add To Cart
+                      </button>
+                      <button to="/signup" className="button signup">
+                        Buy It Now
+                      </button>
+                    </div>
+                  </div>
+                  <div className="d-flex align-items-center gap-15">
+                    <div>
+                      <a href="">
+                        <TbGitCompare className="fs-5 me-2" />
+                        Add To Compare
+                      </a>
+                    </div>
+                    <div>
+                      <a href="">
+                        <FaRegHeart className="fs-5 me-2" />
+                        Add To Wishlist
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -22,8 +169,8 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
+              <h4>Description</h4>
               <div className="bg-white p-3">
-                <h4>Description</h4>
                 <p>
                   Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                   Impedit mollitia vel atque, omnis ad harum hic alias
@@ -35,31 +182,85 @@ const SingleProduct = () => {
           </div>
         </div>
       </div>
-      <section className="home-wrapper-2 py-5 reviews-wrapper">
+      <section className="home-wrapper-2 reviews-wrapper">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <div className="review-head d-flex justify-content-between align-items-end">
-                <div>
-                  <h4 className="mb-2">Customer Reviews</h4>
-                  <div className="d-flex align-items-center gap-10">
-                    <ReactStars
-                      count={5}
-                      size={24}
-                      value={3}
-                      edit={false}
-                      activeColor="#ffd700"
-                    />
-                    <p className="mb-0">Based on 2 Reviews</p>
+              <h3 id="review">Reviews</h3>
+              <div className="review-inner-wrapper">
+                <div className="review-head d-flex justify-content-between align-items-end">
+                  <div>
+                    <h4 className="mb-2">Customer Reviews</h4>
+                    <div className="d-flex align-items-center gap-10">
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <p className="mb-0">Based on 2 Reviews</p>
+                    </div>
+                  </div>
+                  {orderedProduct && (
+                    <div>
+                      <a
+                        href=""
+                        className="text-dark text-decoration-underline"
+                      >
+                        Write a Review
+                      </a>
+                    </div>
+                  )}
+                </div>
+                <div className="review-form py-4">
+                  <h4>Write a Review</h4>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        edit={true}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comment"
+                      ></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <button className="button border-0">Submit Review</button>
+                    </div>
+                  </form>
+                </div>
+                <div className="reviews mt-4">
+                  <div className="review">
+                    <div className="d-flex gap-10 align-items-center">
+                      <h4 className="mb-0">Navdeep</h4>
+                      <ReactStars
+                        count={5}
+                        size={24}
+                        value={3}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <p className="mt-3">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                      Tempora saepe molestiae commodi velit officiis obcaecati
+                      cum, beatae dignissimos aperiam et id veritatis! Magnam
+                      fuga deleniti, dolore eaque architecto consequuntur
+                      blanditiis.
+                    </p>
                   </div>
                 </div>
-                {orderedProduct && (
-                  <div>
-                    <a href="" className="text-dark text-decoration-underline">
-                      Write a Review
-                    </a>
-                  </div>
-                )}
               </div>
             </div>
           </div>
